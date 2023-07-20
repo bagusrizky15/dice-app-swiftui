6919011abf55f7144d518d6ffc4e25cdd5b6b6d5
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-20 09:53:48 +0700

    Initial Commit

---
+++ dice-app-use-swift-ui/dice_app_use_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_app_use_swift_uiApp.swift
+//  dice-app-use-swift-ui
+//
+//  Created by bagus on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct dice_app_use_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
