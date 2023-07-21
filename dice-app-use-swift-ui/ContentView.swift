e199243805d163353f71a09fcf0968bca0244acc
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-21 13:28:58 +0700

    upload

--- dice-app-use-swift-ui/ContentView.swift
+++ dice-app-use-swift-ui/ContentView.swift
@@ -8,19 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var left = 1
+    @State var right = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n:left)
+                    DiceView(n:right)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.left = Int.random(in: 1...6)
+                    self.right = Int.random(in: 1...6)
+                }){
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n : Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
